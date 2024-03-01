031b74ab5999304ac3216ff21d80d8bf80c0e64b
Kummari Tejaswini <tejaswinikummari01@gmail.com>
2024-03-01 22:47:07 +0530

    React Project 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import TicTacToe from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
